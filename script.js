cf646fd1b45fd8e68732ed1c8c47ae5b778e2790
Jakubs1 <jakub.swierczek@microsoft.wsei.edu.pl>
2020-04-18 22:53:21 +0200

    notes page added, first structure

---
+++ script.js
@@ -0,0 +1,82 @@
+var Board = /** @class */ (function () {
+    function Board(header, container) {
+        this.tasks = [];
+        this.title = header;
+        this.tasksContainer = container;
+    }
+    Board.prototype.taskCreate = function () {
+        var content = prompt("Nazwij karteczkę");
+        if (content != null) {
+            var task = new Task(content);
+            this.tasks.push(task);
+            this.drawTask(task);
+        }
+    };
+    Board.prototype.drawTask = function (task) {
+        var taskSection = document.createElement("div");
+        var taskDescription = document.createElement("p");
+        taskSection.className = "taskSection";
+        taskDescription.className = "taskDescription";
+        taskDescription.innerHTML = task.description;
+        taskSection.appendChild(taskDescription);
+        this.tasksContainer.appendChild(taskSection);
+    };
+    return Board;
+}());
+var Task = /** @class */ (function () {
+    function Task(content) {
+        this.description = content;
+    }
+    return Task;
+}());
+var Kanban = /** @class */ (function () {
+    function Kanban(container) {
+        this.boards = [];
+        this.notesSection = document.createElement("div");
+        this.pageAddButton = document.createElement("button");
+        this.boardsContainer = container;
+    }
+    Kanban.prototype.initialBoard = function () {
+        var title = prompt("Dodaj nazwę kolumny");
+        if (title != null) {
+            this.drawBoard(title);
+            var board_1 = new Board(title, this.notesSection);
+            this.boards.push(board_1);
+            this.pageAddButton.addEventListener("click", function () { return board_1.taskCreate(); });
+        }
+    };
+    Kanban.prototype.removeBoard = function () {
+        var title = prompt("Usuń kolumnę o nazwie");
+        if (title != null) {
+            // const index = this.boards.indexOf();
+            // this.boards.splice(index, 1);
+        }
+    };
+    Kanban.prototype.drawBoard = function (title) {
+        var singleBoard = document.createElement("div");
+        var titleSection = document.createElement("div");
+        var boardTitle = document.createElement("h1");
+        titleSection.className = "titleSection";
+        this.notesSection.className = "notesSection";
+        boardTitle.className = "boardTitle";
+        singleBoard.className = "singleBoard";
+        this.pageAddButton.id = "pageAddButton";
+        this.pageAddButton.innerHTML = "Dodaj kartkę";
+        boardTitle.innerHTML = title;
+        titleSection.appendChild(boardTitle);
+        singleBoard.appendChild(titleSection);
+        this.notesSection.appendChild(this.pageAddButton);
+        singleBoard.appendChild(this.notesSection);
+        this.boardsContainer.appendChild(singleBoard);
+    };
+    return Kanban;
+}());
+document.addEventListener("DOMContentLoaded", appStart);
+function appStart() {
+    var boardsContainer = document.getElementById("boards");
+    var addBoardButton = document.getElementById("addBoardButton");
+    var removeBoardButton = document.getElementById("removeBoardButton");
+    var kanban = new Kanban(boardsContainer);
+    addBoardButton.addEventListener('click', function () { return kanban.initialBoard(); });
+    removeBoardButton.addEventListener('click', function () { return kanban.removeBoard(); });
+}
--- script.ts
+++ script.ts
@@ -1,11 +1,33 @@
 class Board {
     title: string;
-    tasks: Task[];
-    constructor(header: string) {
+    tasks: Task[] = [];
+    tasksContainer: HTMLDivElement;
+    constructor(header: string, container: HTMLDivElement) {
         this.title = header;
+        this.tasksContainer = container;
     }
-}
 
+    taskCreate() {
+        let content = prompt("Nazwij karteczkę");
+        if (content != null) {
+            let task: Task = new Task(content);
+            this.tasks.push(task);
+            this.drawTask(task);
+
+        }
+    }
+
+    drawTask(task: Task) {
+        const taskSection = document.createElement("div");
+        const taskDescription = document.createElement("p");
+        taskSection.className = "taskSection";
+        taskDescription.className = "taskDescription";
+        taskDescription.innerHTML = task.description;
+        taskSection.appendChild(taskDescription);
+        this.tasksContainer.appendChild(taskSection);
+    }
+
+}
 class Task {
     description: string;
     constructor(content: string) {
@@ -14,32 +36,54 @@ class Task {
 }
 
 class Kanban {
-    boards: Board[];
+    boards: Board[] = [];
     boardsContainer: HTMLDivElement;
+    notesSection = document.createElement("div");
+    pageAddButton = document.createElement("button");
     constructor(container: HTMLDivElement) {
         this.boardsContainer = container;
     }
     initialBoard() {
         let title = prompt("Dodaj nazwę kolumny");
         if (title != null) {
-            let board: Board = new Board(title);
+            this.drawBoard(title);
+            let board: Board = new Board(title, this.notesSection);
             this.boards.push(board);
-            this.drawBoard();
+            this.pageAddButton.addEventListener("click", () => board.taskCreate());
+
         }
     }
 
     removeBoard() {
         let title = prompt("Usuń kolumnę o nazwie");
         if (title != null) {
-            const index = this.boards.indexOf();
-            this.boards.splice(index, 1);
+            // const index = this.boards.indexOf();
+            // this.boards.splice(index, 1);
         }
     }
 
-    drawBoard() {
+    drawBoard(title) {
+        const singleBoard = document.createElement("div")
+        const titleSection = document.createElement("div");
+        const boardTitle = document.createElement("h1");
+        titleSection.className = "titleSection";
+        this.notesSection.className = "notesSection";
+        boardTitle.className = "boardTitle";
+        singleBoard.className = "singleBoard";
+        this.pageAddButton.id = "pageAddButton";
+        this.pageAddButton.innerHTML = "Dodaj kartkę";
+        boardTitle.innerHTML = title;
+        titleSection.appendChild(boardTitle);
+        singleBoard.appendChild(titleSection);
+        this.notesSection.appendChild(this.pageAddButton);
+        singleBoard.appendChild(this.notesSection);
+        this.boardsContainer.appendChild(singleBoard);
+
 
     }
 }
+
+
 
 document.addEventListener("DOMContentLoaded", appStart);
 
@@ -48,7 +92,7 @@ function appStart() {
     let addBoardButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("addBoardButton");
     let removeBoardButton: HTMLButtonElement = <HTMLButtonElement>document.getElementById("removeBoardButton");
     let kanban: Kanban = new Kanban(boardsContainer);
-    addBoardButton.addEventListener('click', () => kanban.initialBoard);
-    removeBoardButton.addEventListener('click', () => kanban.removeBoard);
+    addBoardButton.addEventListener('click', () => kanban.initialBoard());
+    removeBoardButton.addEventListener('click', () => kanban.removeBoard());
 
 }
